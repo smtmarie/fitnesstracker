0979694d7120a09e5a22d7315944b5ae0609c642
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-14 17:08:51 -0600

    more progress

--- routes/api.js
+++ routes/api.js
@@ -40,7 +40,17 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.get("/api/workouts/range")
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+  .then(dbWorkouts => {
+    console.log(dbWorkouts)
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+
+});
 
 
 
