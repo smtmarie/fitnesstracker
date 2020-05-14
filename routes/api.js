3dd2618c4d62bc67cc277174906665f6cbf30e6e
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-14 17:30:32 -0600

    finishing up api js

--- routes/api.js
+++ routes/api.js
@@ -52,22 +52,15 @@ router.get("/api/workouts/range", (req, res) => {
 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+  .then(() => {
+    res.json(true);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
 
 
 module.exports = router;
