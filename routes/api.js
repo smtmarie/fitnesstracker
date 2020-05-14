9311e3e655426473c516e62d302570e35a06abdd
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-14 17:05:49 -0600

    more api js work

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,44 @@ router.post("/api/workouts", (req, res) => {
 
     Workout.create({})
 
-    .then(dbWorkout => {res.json(dbWorkout)})
+        .then(dbWorkout => { res.json(dbWorkout) })
 
-    .catch(err => {res.json(err)})
+        .catch(err => { res.json(err) })
 })
 
-router.put("/api/workouts/:id",  )
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+
+    Workout.findByIdAndUpdate(
+
+        params.id,
+        { $push: { exercises: body } },
+
+        { new: true, runValidators: true }
+
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+
+    Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range")
+
+
 
 
 
