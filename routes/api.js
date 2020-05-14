06f48e9288a9fb4887752905c982500ef861e24b
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-14 16:40:44 -0600

    routes set up and syntax

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+
+    Workout.create({})
+    
+    .then(dbWorkout => {res.json(dbWorkout)})
+
+    .catch(err => {res.json(err)})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
