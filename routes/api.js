9ed6e539aa71479c49985e6bc78f40e8659485a7
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-14 16:54:30 -0600

    routes files work

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,13 @@ const Workout = require("../models/workout.js");
 router.post("/api/workouts", (req, res) => {
 
     Workout.create({})
-    
+
     .then(dbWorkout => {res.json(dbWorkout)})
 
     .catch(err => {res.json(err)})
 })
+
+router.put("/api/workouts/:id",  )
 
 
 
