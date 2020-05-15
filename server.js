9407822081fa74c5b06646c9e2e7caacde072f95
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-15 09:36:35 -0600

    more work on server js for heroku

--- server.js
+++ server.js
@@ -13,23 +13,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// HEROKU DEPLOYMENT
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://localhost/workout",{
 
+  useNewUrlParser: true,
+  useFindAndModify: false
 
-// mongoose.connect("mongodb://localhost/workout",{
+});
 
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-
-// });
-
-app.use(require("./routes/api"));
-app.use(require("./routes/view"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
 
-  console.log(`App running on port ${PORT}`);
+  console.log(`App running on port ${PORT}!`);
 });
  
+// HEROKU DEPLOYMENT
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI);
