e0acee9f7975585df293845cfe576154b0c89ccd
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-19 08:43:20 -0600

    debugging for heroku deploy

--- routes/api.js
+++ routes/api.js
@@ -1,66 +1,54 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/workout");
 
 router.post("/api/workouts", (req, res) => {
 
-    Workout.create({})
+  Workout.create({})
 
-        .then(dbWorkout => { res.json(dbWorkout) })
+    .then(dbWorkout => { res.json(dbWorkout) })
 
-        .catch(err => { res.json(err) })
-})
-
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-
-    Workout.findByIdAndUpdate(
-
-        params.id,
-        { $push: { exercises: body } },
-
-        { new: true, runValidators: true }
-
-    )
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-
-        });
+    .catch(err => { res.status(500).json(err) });
 });
+
 
 router.get("/api/workouts", (req, res) => {
 
-    Workout.find()
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  Workout.find({})
+
+    .then(dbWorkouts => { res.json(dbWorkouts) })
+
+    .catch(err => { res.status(500).json(err) });
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  Workout.find({}).limit(7)
-  .then(dbWorkouts => {
-    console.log(dbWorkouts)
-    res.json(dbWorkouts);
-  })
-  .catch(err => {
-    res.json(err);
-  });
+  Workout.find({}).limit(8)
+    .then(dbWorkouts => { res.json(dbWorkouts) })
+    .catch(err => { res.status(500).json(err) })
 
 });
 
 router.delete("/api/workouts", ({ body }, res) => {
   Workout.findByIdAndDelete(body.id)
-  .then(() => {
-    res.json(true);
-  })
-  .catch(err => {
-    res.json(err);
-  });
+    .then()
+    .catch(err => { res.status(500).json(err) });
 });
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+
+  Workout.findByIdAndUpdate(
+
+    params.id,
+    { $push: { exercises: body } },
+
+    { new: true, runValidators: true }
+
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => { res.status(500).json(err) });
+});
+
 
 
 module.exports = router;
