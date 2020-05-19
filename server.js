87d1b62fcea8fc53c3a23a21cba8864e4702769c
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-19 08:48:34 -0600

    heroku deploy connect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -13,21 +13,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout",{
+// HEROKU DEPLOYMENT
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI);
 
-  useNewUrlParser: true,
-  useFindAndModify: false
 
-});
+// mongoose.connect("mongodb://localhost/workout",{
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+
+// });
+
+app.use(require("./routes/api"));
+app.use(require("./routes/view"));
 
 app.listen(PORT, () => {
 
   console.log(`App running on port ${PORT}!`);
 });
  
-// HEROKU DEPLOYMENT
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI);
