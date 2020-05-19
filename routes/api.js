ebe221a3b408f8ca08041fdde45da1a9277a0a29
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-05-19 11:55:17 -0600

    more fixes

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,33 @@ router.post("/api/workouts", (req, res) => {
     .catch(err => { res.json(err) });
 });
 
+router.get("/api/workouts", (req, res) => {
+
+  Workout.find({})
+
+    .then(dbWorkouts => { res.json(dbWorkouts) })
+
+    .catch(err => { res.json(err) });
+});
+
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+
+    .then(dbWorkouts => { res.json(dbWorkouts) })
+
+    .catch(err => { res.json(err); })
+
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then() 
+    .catch(err => { res.json(err) });
+
+});
+
+
 router.put("/api/workouts/:id", ({ body, params }, res) => {
 
   Workout.findByIdAndUpdate(
@@ -20,41 +47,8 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
     { new: true, runValidators: true }
 
   )
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => { res.json(err); });
-});
-
-
-router.get("/api/workouts", (req, res) => {
-
-  Workout.find({})
-
-    .then(dbWorkouts => { res.json(dbWorkouts) })
-
+    .then(dbWorkout => { res.json(dbWorkout) })
     .catch(err => { res.json(err) });
-});
-
-router.get("/api/workouts/range", (req, res) => {
-  Workout.find({}).limit(7)
-    .then(dbWorkouts => {
-
-      console.log(dbWorkouts)
-      res.json(dbWorkouts);
-    })
-    .catch(err => { res.json(err); })
-
-});
-
-router.delete("/api/workouts", ({ body }, res) => {
-  Workout.findByIdAndDelete(body.id)
-    .then(() => {
-
-      res.json(true);
-
-    })
-    .catch(err => { res.json(err); });
 });
 
 
